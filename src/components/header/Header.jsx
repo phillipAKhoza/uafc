61b6e355acfd345822d4de4b686fc20d75b05e26
Phillip Khoza <phillipantonk@gmail.com>
2022-05-04 19:04:21 +0200

    bootstrap

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <div>
+            <nav class="navbar fixed-top navbar-light bg-light">
+             <a class="navbar-brand" href="123">Fixed top</a>
+            </nav>
+        </div>
+    )
+}
