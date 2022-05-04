8bfbcf7d005ae4d4c882ec3a5d14d7c2603e1c68
Phillip Khoza <phillipantonk@gmail.com>
2022-05-04 19:06:51 +0200

    home page

---
+++ src/components/body/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Header from '../header/Header'
+export default class Home extends Component{
+    render(){
+        return(
+            <div>
+                <Header/>
+                <h1>HOME SCREEN</h1>
+            </div>
+        )
+    }
+} 
