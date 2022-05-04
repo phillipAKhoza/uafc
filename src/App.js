e379f56488df65243322a136f68678cfe7a8b7b6
Phillip Khoza <phillipantonk@gmail.com>
2022-05-04 19:05:45 +0200

    including bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import {createBrowserHistory} from 'history';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Home from './components/body/Home';
+import 'bootstrap/dist/css/bootstrap.css';
+// const appHistory = {
+//   basename: process.env.PUBLIC_URL
+// };
+// const history = createBrowserHistory(appHistory);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
